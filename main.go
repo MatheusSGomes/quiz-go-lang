b13fc6f32517560b2f6d48a0c257e540a1671faf
Matheus Gomes <math.s.gomes@gmail.com>
2025-03-03 22:22:11 -0300

    add structs questions and game state

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+type Question struct {
+	Text 	string
+	Options []string
+	Answer 	int
+}
+
+type GameState struct {
+	Name 		string
+	Points 		string
+	Questions	[]Question
+}
 
 func main() {
-	fmt.Println("Olá mundo!")
 }
